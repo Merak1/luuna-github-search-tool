4071ef94a9faaa0b381c2c210809e360e7b43052
Merak1 <eddie_comes_for_you@hotmail.es>
2019-12-17 20:39:22 -0600

    Updates

---
+++ precache-manifest.25c1fb4e2d6bc4af2c844fadbabfef78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1fec0dcdc409075f6bff167e4967166",
+    "url": "/luuna-github-search-tool/index.html"
+  },
+  {
+    "revision": "3b3e53f220035ace6696",
+    "url": "/luuna-github-search-tool/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ad3ed0e9d88655f315c5",
+    "url": "/luuna-github-search-tool/static/js/2.083c250d.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/luuna-github-search-tool/static/js/2.083c250d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3b3e53f220035ace6696",
+    "url": "/luuna-github-search-tool/static/js/main.bc63404d.chunk.js"
+  },
+  {
+    "revision": "c511278e6fe02bab6167",
+    "url": "/luuna-github-search-tool/static/js/runtime-main.63b0725c.js"
+  }
+]);
